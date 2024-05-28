af95609f762342217ff433d390852a44fce33994
Jorge Alexandre <jorgeaamsantos@gmail.com>
2024-05-28 16:24:02 -0300

    add product

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import productsData from "../data/products_mock.json";
+import Product from "./Product";
 
 export default function Catalog() {
-  return (
-    <div>Catalog</div>
-  )
+    return (
+        <div>
+            <h1>Catálogo de Produtos</h1>
+            <div className="products-container">
+                {productsData.map((product) => (
+                    <Product key={product.id} product={product} />
+                ))}
+            </div>
+        </div>
+    )
 }
