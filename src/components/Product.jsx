a79bc363f29692a25b0f6f83a6f6ef0a0dbbe564
Jorge Alexandre <jorgeaamsantos@gmail.com>
2024-06-11 16:29:40 -0300

    correcao do nome dos produtos

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -8,7 +8,7 @@ export default function Product({ product, onAddToCart }) {
     return (
         <div className="product">
             <img src={product.image} alt={product.name} />
-            <h3>{product.title}</h3>
+            <h3>{product.name}</h3>
             <p>${product.price}</p>
             <div className="cart-buttons">
                 <select onChange={(e) => setQuantity(e.target.value)}>
