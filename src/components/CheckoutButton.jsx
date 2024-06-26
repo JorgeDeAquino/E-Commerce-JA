afb3eb3600cb3ed2b743e3c3317c7bc37012d68a
Jorge Alexandre <jorgeaamsantos@gmail.com>
2024-06-26 16:51:06 -0300

    corrigindo oncheck

--- src/components/CheckoutButton.jsx
+++ src/components/CheckoutButton.jsx
@@ -1,6 +1,6 @@
 
 
-export default function CheckoutButton( onCheckout ) {
+export default function CheckoutButton({ onCheckout }) {
     return (
         <button onClick={onCheckout}>Finalizar Compra</button>
     )
